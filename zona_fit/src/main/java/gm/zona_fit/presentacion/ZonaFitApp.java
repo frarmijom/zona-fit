e271c7a6f1e09993eca38cbf517b9346912b5d7e
farmijo <farmijo@mail.com>
2025-08-31 10:55:44 -0400

    Avanzando en el proyecto.

---
+++ zona_fit/src/main/java/gm/zona_fit/presentacion/ZonaFitApp.java
@@ -0,0 +1,118 @@
+package gm.zona_fit.presentacion;
+
+import gm.zona_fit.datos.ClienteDAO;
+import gm.zona_fit.datos.IClienteDAO;
+import gm.zona_fit.dominio.Cliente;
+
+import java.util.Scanner;
+
+public class ZonaFitApp {
+    public static void main(String[] args) {
+        zonaFitApp();
+    }
+
+    private static void zonaFitApp() {
+        var salir = false;
+        var consola = new Scanner(System.in);
+        // Creamos un objeto de la clase clienteDao
+        IClienteDAO clienteDao = new ClienteDAO();
+        while (!salir){
+            try{
+              var opcion = mostrarMenu(consola);
+                salir = ejecutarOpciones(consola,opcion, clienteDao);
+            } catch (Exception e) {
+                System.out.println("Error al ejecutar opciones: " + e.getMessage());
+            }
+            System.out.println();
+        }
+    }
+
+    private static int mostrarMenu(Scanner consola) {
+        System.out.print("""
+                *** Zona Fit (GYM)
+                1. Listar Clientes
+                2. Buscar Cliente
+                3. Agregar Cliente
+                4. Modificar Cliente
+                5. Eliminar Cliente
+                6. Salir
+                Elije una opcion:\s""");
+        return Integer.parseInt(consola.nextLine());
+
+    }
+
+    private static boolean ejecutarOpciones(Scanner consola, int opcion, IClienteDAO clienteDao) {
+        var salir = false;
+        switch (opcion){
+            case 1 -> { // 1. Listar clientes
+                System.out.println("--- Listado de Clientes ---");
+                var clientes = clienteDao.listarClientes();
+                clientes.forEach(System.out::println);
+            }
+            case 2 ->{
+                System.out.println("Introduce el id del cliente a buscar: ");
+                var idCliente = Integer.parseInt(consola.nextLine());
+                var cliente = new Cliente(idCliente);
+                var encontrado = clienteDao.buscarClientePorId(cliente);
+                if (encontrado)
+                    System.out.println("Cliente encontrado: " + cliente);
+                else
+                    System.out.println("Cliente no encontrado: " + cliente);
+            }
+            case 3 ->{
+                System.out.println("--- agregar cliente ---");
+                System.out.print("Nombre: ");
+                var nombre = consola.nextLine();
+                System.out.print("Apellido: ");
+                var apellido = consola.nextLine();
+                System.out.print("Membresia: ");
+                var membresia = Integer.parseInt(consola.nextLine());
+                // Creamos el objeto cliente, sin el id
+                var cliente = new Cliente(nombre,apellido,membresia);
+                var agregado = clienteDao.agregarCliente(cliente);
+                if (agregado)
+                    System.out.println("Cliente agregado: " + cliente);
+                else
+                    System.out.println("Cliente NO agregado: " + cliente );
+            }
+            case 4 ->{
+                System.out.println("--- Modificar Cliente ---");
+                System.out.print("Id cliente: ");
+                var idCliente = Integer.parseInt(consola.nextLine());
+                System.out.print("Nombre: ");
+                var nombre = consola.nextLine();
+                System.out.print("Apellido: ");
+                var apellido = consola.nextLine();
+                System.out.print("Membresia: ");
+                var membresia = Integer.parseInt(consola.nextLine());
+                // Creamos el objeto a modificar
+                var cliente = new Cliente(idCliente,nombre,apellido,membresia);
+                var modificado = clienteDao.modificarCliente(cliente);
+                if (modificado)
+                    System.out.println("Cliente modificado: " + cliente);
+                else
+                    System.out.println("Cliente NO modificado");
+            }
+            case 5 ->{
+                System.out.println("--- Eliminar Cliente ---");
+                System.out.print("Id cliente: ");
+                var idCliente = Integer.parseInt(consola.nextLine());
+                // Creamos el objeto a modificar
+                var cliente = new Cliente(idCliente);
+                var eliminado = clienteDao.eliminarCiente(cliente);
+                if (eliminado)
+                    System.out.println("Cliente eliminado: " + cliente);
+                else
+                    System.out.println("Cliente NO modificado");
+            }
+            case 6 ->{
+                System.out.println("Hasta pronto!");
+                salir=true;
+            }
+            default -> System.out.println("Opcion no reconocida: " + opcion);
+
+        }
+        return salir;
+    }
+
+}
